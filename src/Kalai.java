c8ddbcbdd2d730b2d3cb58f23203337893e6609d
Trinh Pham <trinhpn@uw.edu>
2018-03-08 00:15:04 -0800

    Second commit. safePrime added

--- src/Kalai.java
+++ src/Kalai.java
@@ -16,29 +16,30 @@ public class Kalai
 	static BigInteger two = new BigInteger("2");
 	static BigInteger upperN;
 	// Number of bits of N
-	static int numBit = 260;
+	static int numBit = 150;
 	public static void main(String[] args) throws IOException {
-
 		long start = System.nanoTime();
 		DateFormat dateFormat = new SimpleDateFormat("HHmmss");
 		Date date = new Date();
 		System.out.println(dateFormat.format(date));
+
 		ArrayList<BigInteger> result = sequenceOfRs(two.pow(numBit).subtract(one), two.pow(numBit-1), numBit);
 		System.out.println("Running time: " + (System.nanoTime() - start)/1000000000.0);
 	    int size = result.size();
 
 		// Write result into an output file
-		String fileName = "output" + numBit + dateFormat.format(date) +".txt";
+		String fileName = "output" + numBit + "bits"+ dateFormat.format(date) +".txt";
 	    PrintWriter out = new PrintWriter(new FileWriter(fileName, true), true);
-	    out.write("The prime is: " + result.get(size - 1) + " ");
+	    out.write("The prime is: " + result.get(size - 1).add(one) + " ");
+	    out.write("Running time: " + (System.nanoTime() - start)/1000000000.0 + " seconds.");
 	    for(BigInteger big : result) {
 	    	out.write("\n ");
 	    	out.write(big.toString());
-
 		}
+
 	    System.out.println("Done finding prime factors of (p-1)");
 	    ArrayList<BigInteger> copy = new ArrayList<BigInteger>(result.subList(0, size - 2));
-	    BigInteger g = (generator(result.get(size-1).add(one), copy));
+	    BigInteger g = generator(result.get(size-1).add(one), copy);
 		System.out.println("Generator g = " + g);
 	    out.write("\n");
 		out.write("Generator g = " + g);
@@ -65,9 +66,14 @@ public class Kalai
 	    	BigInteger g = randomR(p, rand);
 	    	if (g.equals(p)) continue;
 	    	boolean temp1 = true;
-	    	for(Integer m : mArray) {
-	    		// temp1 = true only if all the g.pow(m) not equal 1
-	    		temp1 &= !g.pow(m).mod(p).equals(one); 
+	    	try {
+		    	for(Integer m : mArray) {
+		    		// temp1 = true only if all the g.pow(m) not equal 1
+		    		temp1 &= !g.pow(m).mod(p).equals(one); 
+		    	}
+	    	} catch (ArithmeticException ex) {
+	    		temp1 = false;
+	    		break;
 	    	}
 	    	// Temp1 true means we found generator, stop while loop 
 	    	done = temp1;
@@ -103,12 +109,11 @@ public class Kalai
 			if (r.isProbablePrime(40) || r.equals(one)) {
 				n = n.multiply(r);
 				// add the r into array
-				result.add(r);
+				if (!r.equals(one)) result.add(r);	
 			}
 			if (r.equals(one) ){
 				if (n.compareTo(lowerN) > -1 && n.compareTo(upperN) < 1 && n.compareTo(m) >= 0 && n.add(one).isProbablePrime(55)) {
 					System.out.println("\nDone");
-					System.out.println("m = " + m);
 					result.add(n);
 					for(BigInteger big : result) {
 						System.err.println(big.toString() + " ");
